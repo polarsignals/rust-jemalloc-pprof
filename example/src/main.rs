491faa479a3193e07fb4731fdd3c2d8cace537ae
Brennan Vincent <brennan@umanwizard.com>
2023-12-15 11:54:57 -0500

    fix example

--- example/src/main.rs
+++ example/src/main.rs
@@ -1,3 +1,6 @@
+use axum::http::StatusCode;
+use axum::response::IntoResponse;
+
 #[cfg(not(target_env = "msvc"))]
 #[global_allocator]
 static ALLOC: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
@@ -13,32 +16,32 @@ async fn main() {
         v.push(i);
     }
 
-    let app = axum::Router::new()
-        .route("/debug/pprof/heap", axum::routing::get(handle_get_heap));
+    let app = axum::Router::new().route("/debug/pprof/heap", axum::routing::get(handle_get_heap));
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
-pub async fn handle_get_heap() -> Result<impl axum::response::IntoResponse, (axum::http::StatusCode, String)> {
+pub async fn handle_get_heap() -> Result<impl IntoResponse, (StatusCode, String)> {
     let mut prof_ctl = jemalloc_pprof::PROF_CTL.as_ref().unwrap().lock().await;
     require_profiling_activated(&prof_ctl)?;
-    let dump_file = prof_ctl
-        .dump()
-        .map_err(|err| (axum::http::StatusCode::INTERNAL_SERVER_ERROR, err.to_string()))?;
-    let dump_reader = std::io::BufReader::new(dump_file);
-    let profile = jemalloc_pprof::parse_jeheap(dump_reader)
-        .map_err(|err| (axum::http::StatusCode::INTERNAL_SERVER_ERROR, err.to_string()))?;
-    let pprof = profile.to_pprof(("inuse_space", "bytes"), ("space", "bytes"), None);
+    let pprof = prof_ctl
+        .dump_pprof()
+        .map_err(|err| (StatusCode::INTERNAL_SERVER_ERROR, err.to_string()))?;
     Ok(pprof)
 }
 
 /// Checks whether jemalloc profiling is activated an returns an error response if not.
-fn require_profiling_activated(prof_ctl: &jemalloc_pprof::JemallocProfCtl) -> Result<(), (axum::http::StatusCode, String)> {
+fn require_profiling_activated(
+    prof_ctl: &jemalloc_pprof::JemallocProfCtl,
+) -> Result<(), (axum::http::StatusCode, String)> {
     if prof_ctl.activated() {
         Ok(())
     } else {
-        Err((axum::http::StatusCode::FORBIDDEN, "heap profiling not activated".into()))
+        Err((
+            axum::http::StatusCode::FORBIDDEN,
+            "heap profiling not activated".into(),
+        ))
     }
 }
